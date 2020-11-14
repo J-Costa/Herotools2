378fd5b3bf90adbddd43f0ad80e1eb673141188b
Josue Costa <juncosta@gmail.com>
2020-11-14 17:43:28 -0300

    teste

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production"){
-    module.expors = {mongoURI: "mongodb+srv://herotools:@Mlab2020@cluster0.bsrny.mongodb.net/ferramentas?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://herotools:@Mlab2020@cluster0.bsrny.mongodb.net/ferramentas?retryWrites=true&w=majority"}
     console.log("Produção")
 } else {
     module.exports = {mongoURI: "mongodb://localhost/herotools"}
